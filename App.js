8a1f241091608dee869f7f74ffcd4878c477fd5f
NAHID12105080 <nahid151341@gmail.com>
2024-12-25 22:41:59 +0600

    use safeAreaView to protect content

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   ScrollView,
   Button,
   Linking,
+  SafeAreaView,
 } from "react-native";
 import { Image } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
@@ -16,75 +17,90 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./assets/nlp1.png")}
-        style={{ width: "100%", height: 200 }}
-      />
-
-      <Image
-        source={require("./assets/nahid.png")}
+    <SafeAreaView style={{ backgroundColor: "black" }}>
+      <View
         style={{
-          width: 100,
-          height: 100,
-          borderRadius: 50,
-          marginTop: -50,
-          borderWidth: 2,
-          borderColor: "yellow",
-          shadowColor: "black",
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.5,
-          shadowRadius: 2,
-          elevation: 5,
+          backgroundColor: "green",
+          height: "100%",
+          alignItems: "center",
         }}
-      />
-
-      <Text style={{ fontSize: 10, fontWeight: "bold" }}>Md Nahid Hassan</Text>
-
-      <View style={{ flexDirection: "row", marginTop: 20 }}>
-        <FontAwesome6
-          name="github"
-          size={24}
-          color="black"
-          style={{ marginHorizontal: 10 }}
-        />
-        <FontAwesome6
-          name="linkedin"
-          size={24}
-          color="black"
-          style={{ marginHorizontal: 10 }}
-        />
-        <FontAwesome6
-          name="twitter"
-          size={24}
-          color="black"
-          style={{ marginHorizontal: 10 }}
-        />
+      >
+        <Text style={{ color: "black", fontSize: 20 }}>Hello World</Text>
       </View>
+    </SafeAreaView>
 
-      <Button title="Get In Touch" onPress={getInTouch} />
+    // <View style={styles.container}>
+    //   <Image
+    //     source={require("./assets/nlp1.png")}
+    //     style={{ width: "100%", height: 200 }}
+    //   />
 
-      {/* <Button title="Click Me" onPress={() => alert("Hello World")} /> */}
+    //   <Image
+    //     source={require("./assets/nahid.png")}
+    //     style={{
+    //       width: 100,
+    //       height: 100,
+    //       borderRadius: 50,
+    //       marginTop: -50,
+    //       borderWidth: 2,
+    //       borderColor: "yellow",
+    //       shadowColor: "black",
+    //       shadowOffset: { width: 0, height: 2 },
+    //       shadowOpacity: 0.5,
+    //       shadowRadius: 2,
+    //       elevation: 5,
+    //     }}
+    //   />
 
-      <ScrollView>
-        <Text style={{ fontSize: 20, margin: 10 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          scelerisque, nunc in ultricies luctus, nulla eros tincidunt orci, ac
-          tincidunt erat ex eu purus. Sed sed nisl nec nisl fermentum
-          sollicitudin. Integer auctor, mi sit amet ultricies interdum, nunc
-          justo tincidunt erat, vel porttitor dui felis a dui. Fusce nec
-          scelerisque odio. Nullam vel purus auctor, tincidunt risus vel,
-          scelerisque mi. Nullam nec metus nec eros ultrices ultricies. Aliquam
-          erat volutpat. Sed nec ante ut justo ultricies tincidunt. Nullam
-          pharetra, dui sit amet malesuada sagittis, ante felis congue justo,
-          nec ultr ices nunc nisl nec nisl. Nullam nec metus nec eros ultrices
-          ultricies. Aliquam erat volutpat. Sed nec ante ut justo ultricies
-          tincidunt. Nullam pharetra dui sit amet malesuada sagittis, ante felis
-        </Text>
-      </ScrollView>
+    //   <Text style={{ fontSize: 10, fontWeight: "bold" }}>
+    //     Md Nahid Hassan
+    //   </Text>
 
-      <StatusBar style="auto" />
-    </View>
+    //   <View style={{ flexDirection: "row", marginTop: 20 }}>
+    //     <FontAwesome6
+    //       name="github"
+    //       size={24}
+    //       color="black"
+    //       style={{ marginHorizontal: 10 }}
+    //     />
+    //     <FontAwesome6
+    //       name="linkedin"
+    //       size={24}
+    //       color="black"
+    //       style={{ marginHorizontal: 10 }}
+    //     />
+    //     <FontAwesome6
+    //       name="twitter"
+    //       size={24}
+    //       color="black"
+    //       style={{ marginHorizontal: 10 }}
+    //     />
+    //   </View>
+
+    //   <Button title="Get In Touch" onPress={getInTouch} />
+
+    //   {/* <Button title="Click Me" onPress={() => alert("Hello World")} /> */}
+
+    //   <ScrollView>
+    //     <Text style={{ fontSize: 20, margin: 10 }}>
+    //       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+    //       scelerisque, nunc in ultricies luctus, nulla eros tincidunt orci, ac
+    //       tincidunt erat ex eu purus. Sed sed nisl nec nisl fermentum
+    //       sollicitudin. Integer auctor, mi sit amet ultricies interdum, nunc
+    //       justo tincidunt erat, vel porttitor dui felis a dui. Fusce nec
+    //       scelerisque odio. Nullam vel purus auctor, tincidunt risus vel,
+    //       scelerisque mi. Nullam nec metus nec eros ultrices ultricies.
+    //       Aliquam erat volutpat. Sed nec ante ut justo ultricies tincidunt.
+    //       Nullam pharetra, dui sit amet malesuada sagittis, ante felis congue
+    //       justo, nec ultr ices nunc nisl nec nisl. Nullam nec metus nec eros
+    //       ultrices ultricies. Aliquam erat volutpat. Sed nec ante ut justo
+    //       ultricies tincidunt. Nullam pharetra dui sit amet malesuada
+    //       sagittis, ante felis
+    //     </Text>
+    //   </ScrollView>
+
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ Notes.md
@@ -0,0 +1,15 @@
+# expo-master-practice
+
+## safeAreaView
+
+- SafeAreaView protects from noches
+- don't take padding
+
+### Use this instead:
+
+```bash
+npx expo install react-native-safe-area-context
+```
+
+- as here one can select which side to choose to safe guard
+-
