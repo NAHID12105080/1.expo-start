8ea42f39f895756d6660e5145beddb50562277d4
NAHID12105080 <nahid151341@gmail.com>
2024-12-25 18:35:33 +0600

    "created a facebook profile image with cover photo"

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require("./assets/nlp1.png")}
+        style={{ width: "100%", height: 200 }}
+      />
+      <Image
+        source={require("./assets/nahid.png")}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          marginTop: -50,
+          borderWidth: 2,
+          borderColor: "yellow",
+          shadowColor: "black",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.5,
+          shadowRadius: 2,
+          elevation: 5,
+        }}
+      />
+
+      <Text style={{ fontSize: 10, fontWeight: "bold" }}>Md Nahid Hassan</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
