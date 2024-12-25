f0be677f224d4599a4e2834a9486db7a684d9627
NAHID12105080 <nahid151341@gmail.com>
2024-12-25 18:50:10 +0600

    added icon from expo-vector-icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Image } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         source={require("./assets/nlp1.png")}
         style={{ width: "100%", height: 200 }}
       />
+
       <Image
         source={require("./assets/nahid.png")}
         style={{
@@ -27,6 +29,28 @@ export default function App() {
       />
 
       <Text style={{ fontSize: 10, fontWeight: "bold" }}>Md Nahid Hassan</Text>
+
+      <View style={{ flexDirection: "row", marginTop: 20 }}>
+        <FontAwesome6
+          name="github"
+          size={24}
+          color="black"
+          style={{ marginHorizontal: 10 }}
+        />
+        <FontAwesome6
+          name="linkedin"
+          size={24}
+          color="black"
+          style={{ marginHorizontal: 10 }}
+        />
+        <FontAwesome6
+          name="twitter"
+          size={24}
+          color="black"
+          style={{ marginHorizontal: 10 }}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
